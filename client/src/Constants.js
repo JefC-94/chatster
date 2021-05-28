39ffd63213048c3f484f8831b63d7ea0d19eec4d
Jef Ceuppens <JefCeuppens@MacBook-Pro-van-Jef.local>
2021-05-28 14:57:02 +0200

    updated readme

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -8,5 +8,5 @@ export const Axios = axios.create({
 
 //IMG PATH ON LOCALHOST AND PRODUCTION ENV, at this time the same as baseUrl but it might change so keep separate variable
 
-export const imgPath = process.env.NODE_ENV === 'development' ? '/server/uploads' : 'https://chatster-production.herokuapp.com/server/uploads';
-//export const imgPath = '/server/uploads';
+//export const imgPath = process.env.NODE_ENV === 'development' ? '/server/uploads' : 'https://chatster-production.herokuapp.com/server/uploads';
+export const imgPath = '/server/uploads';
