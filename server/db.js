ce71fc950420122d5a4564fe157a47111f3546f2
Jef Ceuppens <JefCeuppens@MacBook-Pro-van-Jef.local>
2021-05-28 14:28:19 +0200

    deleted db credentials

--- server/db.js
+++ server/db.js
@@ -1,15 +1,4 @@
 
-/* const knex = require('knex')({
-    client: 'mysql2',
-    connection: {
-        host: 'ID348579_messagingappdev.db.webhosting.be',
-        user: 'ID348579_messagingappdev',
-        password: 'pur3r4nd0mn3ss',
-        database: 'ID348579_messagingappdev',
-        port: 3306,
-    }
-}); */
-
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
